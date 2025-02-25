3b6ad0d26f766756ad89b43851382ad2ec07dfa1
Chibuzor <chibuzormekalam@gmail.com>
2025-02-25 06:08:20 +0100

    added delete favotite cart function

--- routers/favouriteCartRouter.js
+++ routers/favouriteCartRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const favouriteCartController = require("../controllers/favouriteCartController");
 router.post("/addfavourite", favouriteCartController.addToFavouriteCart);
 router.get("/getfacouriteCart/:name", favouriteCartController.getFavourite);
+router.delete("/deletefacouriteCart/:name", favouriteCartController.removeFavouriteMovie);
 
 module.exports = router;
