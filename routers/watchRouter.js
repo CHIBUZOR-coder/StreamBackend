29419fc0a7b8941b14010af7967f74d9200cddbb
Chibuzor <chibuzormekalam@gmail.com>
2025-02-26 07:08:57 +0100

    adding getWatchCount router

--- routers/watchRouter.js
+++ routers/watchRouter.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const watchController = require("../controllers/watchController");
 
-
-
 router.post("/addwatchCount", watchController.addToWatchCount);
+router.get("/addwatchCount", watchController.getWatchCount);
 
 module.exports = router;
