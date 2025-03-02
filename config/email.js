3410ceb52ad253ed343f150d4bfded51fe45097b
Chibuzor <chibuzormekalam@gmail.com>
2025-03-02 02:26:17 +0100

    fixing transporter

--- config/email.js
+++ config/email.js
@@ -8,12 +8,26 @@ const pass = process.env.EMAIL_HOST_PASSWORD;
 // console.log("host:", host);
 // console.log("pass:", pass);
 
+// const transporter = nodemailer.createTransport({
+//   service: process.env.EMAIL_HOST,
+//   auth: {
+//     user: process.env.EMAIL_HOST_USER,
+//     pass: process.env.EMAIL_HOST_PASSWORD,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_HOST,
+  host: "smtp.gmail.com",
+  port: 587, // Use 465 if using SSL
+  secure: false, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_HOST_USER,
     pass: process.env.EMAIL_HOST_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 module.exports = transporter;
